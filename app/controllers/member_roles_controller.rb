24920d79d30f39b8fe6ee6b5bd62fe45566c8c3b
Eduardo Alvarez Hernandez <eduardo_alvz@hotmail.com>
2024-03-20 16:45:37 -0500

    Added policy for member roles

--- app/controllers/member_roles_controller.rb
+++ app/controllers/member_roles_controller.rb
@@ -2,6 +2,7 @@ class MemberRolesController < ApplicationController
   before_action :set_member_role, only: %i[ show edit update destroy ]
 
   def index
+    authorize MemberRole
     @member_roles = MemberRole.all
     if params[:query].present?
       @member_roles = @member_roles.joins(:member, :role).where("members.first_name ILIKE ? OR members.last_name ILIKE ? OR roles.name ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%")
@@ -22,19 +23,23 @@ class MemberRolesController < ApplicationController
 
   # GET /member_roles/1 or /member_roles/1.json
   def show
+    authorize MemberRole
   end
 
   # GET /member_roles/new
   def new
+    authorize MemberRole
     @member_role = MemberRole.new
   end
 
   # GET /member_roles/1/edit
   def edit
+    authorize MemberRole
   end
 
   # POST /member_roles or /member_roles.json
   def create
+    authorize MemberRole
     @member_role = MemberRole.new(member_role_params)
 
     respond_to do |format|
@@ -50,6 +55,7 @@ class MemberRolesController < ApplicationController
 
   # PATCH/PUT /member_roles/1 or /member_roles/1.json
   def update
+    authorize MemberRole
     respond_to do |format|
       if @member_role.update(member_role_params)
         format.html { redirect_to member_role_url(@member_role), notice: "Member role was successfully updated." }
@@ -63,6 +69,7 @@ class MemberRolesController < ApplicationController
 
   # DELETE /member_roles/1 or /member_roles/1.json
   def destroy
+    authorize MemberRole
     @member_role.destroy!
 
     respond_to do |format|
