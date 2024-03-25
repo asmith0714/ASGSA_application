0203004965cd80a19bddc10375105f172ae004ef
Nicholas Nguyen <nick.nguyen1@tamu.edu>
2024-03-25 02:31:56 -0500

    Added category and archive to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_220000) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_080529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_220000) do
     t.datetime "updated_at", null: false
     t.string "contact_info"
     t.string "category"
+    t.boolean "archive"
   end
 
   create_table "member_notifications", primary_key: "member_notification_id", force: :cascade do |t|
