7b97997d277720518885765b5eef714d947794f2
Nicholas Nguyen <nick.nguyen1@tamu.edu>
2024-03-25 01:49:03 -0500

    Added a filter, archive functions, and new category param

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,10 +5,20 @@ class EventsController < ApplicationController
     @events = Event.all
     @events = @events.search(params[:query]) if params[:query].present?
     @pagy, @events = pagy @events.reorder(sort_column => sort_direction), items: params.fetch(:count, 10)
+
+    case params[:filter]
+    when "Past Events"
+      @events = @events.where("date < ? AND archive = ?", DateTime.now, false)
+    when "Archived Events"
+      @events = @events.where(archive: true)
+    else # Default to "Upcoming Events"
+      @events = @events.where("date >= ? AND archive = ?", DateTime.now, false)
+    end
   end
 
+  # FIX NEED TO BE SORTED BY DATE AND TIME, ONLY DATE RIGHT NOW
   def sort_column
-    %w{ name date start_time end_time capacity points }.include?(params[:sort]) ? params[:sort] : "date"
+    %w{ name date start_time end_time category capacity points }.include?(params[:sort]) ? params[:sort] : 'date'
   end
 
   def sort_direction
@@ -77,6 +87,18 @@ class EventsController < ApplicationController
     end
   end
 
+  def toggle_archive
+    @event = Event.find(params[:id])
+    @event.update(archive: !@event.archive)
+    redirect_back(fallback_location: events_path)
+  end
+
+  def archive_past_events
+    @events = Event.where("date < ? AND archive = ?", DateTime.now, false)
+    @events.update_all(archive: true)
+    redirect_to events_url
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
@@ -95,10 +117,9 @@ class EventsController < ApplicationController
       :category,
       :description,
       :capacity,
-      :points
+      :points,
+      :archive
     )
   end
-
-
 
 end
