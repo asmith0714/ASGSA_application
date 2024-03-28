98a461812375e336708045e1d5eb4be2c653ad17
Eduardo Alvarez Hernandez <eduardo_alvz@hotmail.com>
2024-03-28 13:32:18 -0500

    Mocked the member_role

--- spec/feature/role_views/member_role_integration_spec.rb
+++ spec/feature/role_views/member_role_integration_spec.rb
@@ -3,32 +3,29 @@ require 'rails_helper'
 RSpec.describe "Member View", type: :feature do
     before do
         Rails.application.load_seed
-    end
-  
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
-        provider: 'google_oauth2',
-        uid: '123456789',
-        info: {
-        email: "john@tamu.edu",
-        first_name: "John",
-        last_name: "Doe",
-        image: "https://example.com/image.jpg"
-        },
-        credentials: {
-        token: "token",
-        refresh_token: "refresh token",
-        expires_at: DateTime.now,
-        }
-    })
-  
-    before do
+        
+        @member1 = create(:member)
+
+        # Setup mock OmniAuth user
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+          provider: 'google_oauth2',
+          uid: '123456789',
+          info: {
+            email: @member1.email,
+            first_name: @member1.first_name,
+            last_name: @member1.last_name,
+            image: @member1.avatar_url
+          },
+          credentials: {
+            token: "token",
+            refresh_token: "refresh token",
+            expires_at: DateTime.now,
+          }
+        })
+    
+        # Route to trigger the OmniAuth callback directly for testing
         visit member_google_oauth2_omniauth_callback_path
-        click_button "Update Profile"
-        visit member_roles_path
-        find('#edit_btn').click
-        select 'Member', from: 'member_role_id'
-        click_button "Update Member's Role"
     end
 
     scenario "Nav Links" do
